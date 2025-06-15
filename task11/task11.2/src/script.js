196f9d2f6aa13301f91b8fd9bfc741bec363213a
Phonker131 <shapovaltim@gmail.com>
2025-06-15 15:21:22 +0200

    Made minor change to task 11.2

--- task11/task11.2/src/script.js
+++ task11/task11.2/src/script.js
@@ -4,6 +4,6 @@ container.addEventListener("click", function (event) {
     if (event.target.tagName === "BUTTON") {
         const btnText = event.target.textContent;
         const btnId = event.target.getAttribute("data-id");
-        console.log(`button that was clicked ${btnText}, data-id = ${btnId}`);
+        alert(`button that was clicked: ${btnText}`);
     }
 });
